cd7d3264d8628e7bb769e1ed3307118356323652
Mohan1803 <89601675+Mohan1803@users.noreply.github.com>
2022-03-28 17:58:13 +0530

    mohan

--- backend/src/services/users/users.hooks.js
+++ backend/src/services/users/users.hooks.js
@@ -1,32 +1,62 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
+const { authenticate } = require("@feathersjs/authentication").hooks;
+const validator = require("validator");
 
-const {
-  hashPassword, protect
-} = require('@feathersjs/authentication-local').hooks;
+const { hashPassword, protect } =
+  require("@feathersjs/authentication-local").hooks;
 
 module.exports = {
   before: {
     all: [],
-    find: [ authenticate('jwt') ],
-    get: [ authenticate('jwt') ],
-    create: [ hashPassword('password') ],
-    update: [ hashPassword('password'),  authenticate('jwt') ],
-    patch: [ hashPassword('password'),  authenticate('jwt') ],
-    remove: [ authenticate('jwt') ]
+    find: [authenticate("jwt")],
+    get: [authenticate("jwt")],
+    create: [
+      hashPassword("password"),
+      async (context) => {
+        let input = context.data;
+        //const userService = app.service('users');
+
+        if (validator.isEmpty(input.first_name)) {
+          throw new Error("Please Enter Your Name");
+        }
+        if (validator.isEmpty(input.emailid)) {
+          throw new Error("Please Enter Your Email ID");
+        } else if (!validator.isEmail(input.emailid)) {
+          throw new Error("Please Enter Valid Email ID");
+        }
+        if (validator.isEmpty(input.password)) {
+          throw new Error("Please Enter Password");
+        }
+
+        await context.service
+          .find({
+            query: {
+              emailid: input.emailid,
+            },
+          })
+          .then((data) => {
+            if (data.data.length) {
+              throw new Error("This Email ID Is Already Present.");
+            }
+          });
+      },
+    ],
+    update: [hashPassword("password"), authenticate("jwt")],
+    patch: [hashPassword("password"), authenticate("jwt")],
+    remove: [authenticate("jwt")],
   },
 
   after: {
-    all: [ 
+    all: [
       // Make sure the password field is never sent to the client
       // Always must be the last hook
-      protect('password')
+      protect("password"),
     ],
     find: [],
     get: [],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
@@ -36,6 +66,6 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
+    remove: [],
+  },
 };
