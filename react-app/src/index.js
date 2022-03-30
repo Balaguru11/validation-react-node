25c98247bfc3dd25120970d381db331bbf32aa3a
Balaguru Mamundi <muraliguru99@gmail.com>
2022-03-30 19:19:37 +0530

    react 18

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,11 +4,20 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//       <App />
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </>
 );
